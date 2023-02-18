e680588594cfd64e08c49f9f65ac58e17a6fac55
Francesco Sullo <sullof@gmail.com>
2023-02-18 15:46:59 -0800

    Minor change to comments

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Protector Protected Protocol
+
 A protocol to manage application NFTs, born from the collaboration between [The Round Table](https://trt.gg), [Nduja Labs](https://ndujalabs.com) and [Everdragons2](https://everdragons2.com).
 
 ## History
 
+**0.1.1**
+
+- Separate ERC721Attributable from IProtector interface for clarity and generality
+
 **0.1.0**
+
 - first version
 
 ## Copyright
--- contracts/interfaces/IERC721Approvable.sol
+++ contracts/interfaces/IERC721Approvable.sol
@@ -3,26 +3,26 @@ pragma solidity ^0.8.17;
 
 // Author: Francesco Sullo <francesco@sullo.co>
 
-// the interfaceId is 0xca298e54
-interface IERC721Approvable {
-  // This interface is used in security related tokens, where
-  // by default a tokens is transferable only by the owner.
-  // However, some tokens can be made approvable, so that
-  // they can be traded on exchanges.
+// This interface is for security related tokens, where
+// by default a tokens is transferable only by the owner, sometimes
+// requiring a second factor (a second signature, etc.).
+// However, some tokens can be made approvable, so that
+// they can be traded on exchanges.
+// The interfaceId is 0xca298e54
 
-  // Must be emitted any time the status changes.
-  // However, since the default status is returned by defaultApprovable
+interface IERC721Approvable {
+  // Must be emitted any time the status changes.  However,
+  // since the default status is returned by defaultApprovable
   // it is not necessary to emit it when the token is minted.
+  // In other words, as long as an Approvable event is not emitted,
+  // for a token ID, the defaultApprovable should be assumed.
   event Approvable(uint256 indexed tokenId, bool approvable);
 
-  // An NFT not approvable by default can be made approvable.
-  // This forces 2 transaction for the first approval, but the
-  // implementation can create a function that does both in
-  // sequence, i.e., in a single transaction.
-  // Must be called by the owner of the NFT.
+  // It makes a token approvable or not approvable.
+  // It must emit the Approvable event.
   function makeApprovable(uint256 tokenId, bool status) external;
 
-  // Returns true if the NFT is approvable.
+  // Returns true if the token is approvable.
   // It should revert if the token does not exist.
   function isApprovable(uint256 tokenId) external view returns (bool);
 
@@ -30,6 +30,10 @@ interface IERC721Approvable {
   function defaultApprovable() external pure returns (bool);
 
   // A contract implementing this interface should not allow
-  // the approval for all. So, any exchange validating this interface
+  // the approval for all. So, any actor validating this interface
   // should assume that the tokens are not approvable for all.
+
+  // An extension of this interface may include info about the
+  // approval for all, but it should be considered as a separate
+  // feature, not as a replacement of this interface.
 }
